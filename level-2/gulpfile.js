75322d7d1db825755af07c929f55d4f42770fe5a
Wojciech Sapiechowski <wojtek.sapiechowski@nurtureagency.com>
2019-08-14 18:11:02 +0200

    Level 2 - done without description

---
+++ level-2/gulpfile.js
@@ -0,0 +1,59 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
+
+// File paths
+const files = { 
+    scssPath: 'scss/**/*.scss',
+    jsPath: 'js/**/*.js'
+}
+
+function scssTask(){    
+    return src(files.scssPath)
+        
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass({
+            includePaths: [
+                'variables/'
+            ]
+        })) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('dist')
+    ); // put final CSS in dist folder
+}
+
+function jsTask(){
+    return src([
+        files.jsPath
+        ])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(dest('dist')
+    );
+}
+
+var cbString = new Date().getTime();
+function cacheBustTask(){
+    return src(['index.html'])
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+        .pipe(dest('.'));
+}
+
+function watchTask(){
+    watch([files.scssPath, files.jsPath], 
+        parallel(scssTask, jsTask));    
+}
+
+exports.default = series(
+    parallel(scssTask, jsTask), 
+    cacheBustTask,
+    watchTask
+);
